6d3769030107d8db262810a389ea2cb70296451b
nicolasdelgadillos <nicolasdelgadillo75@gmail.com>
2019-08-31 13:20:37 -0400

    Web api

---
+++ mvcPhone/apiPhone/Models/Phone.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace apiPhone.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum TypeContact
+    {
+        PhoneNumber,
+        Email,
+        Facebook,
+        Twitter,
+        LinkedIN
+    }
+    public class Phone
+    {
+        [Key]
+        public int PhoneId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public TypeContact Type { get; set; }
+        [Required]
+        public string Contact { get; set; }
+
+    }
+
+}
